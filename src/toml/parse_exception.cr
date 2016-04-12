01119bedf59f6499b7fd848a1190c9e35840a319
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-04-11 23:12:37 -0300

    Updated to Crystal > 0.15.0

--- src/toml/parse_exception.cr
+++ src/toml/parse_exception.cr
@@ -1,10 +1,10 @@
 # Raised on invalid TOML strings.
 class TOML::ParseException < Exception
   # The line number where the invalid TOML was detected.
-  getter line_number
+  getter line_number : Int32
 
   # The column number where the invalid TOML was detected.
-  getter column_number
+  getter column_number : Int32
 
   # Creates a ParseException with the given message, line number and column number.
   def initialize(message, @line_number, @column_number)
